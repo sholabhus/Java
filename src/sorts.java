7c5f92138fbd9c0c4779f3be17391ea7b8075d20
sholaabhus <sholaabhus@gmail.com>
2021-02-14 21:49:41 +0000

    SelectionSort -updates

--- src/sorts.java
+++ src/sorts.java
@@ -6,7 +6,7 @@ public class sorts {
     public static void main(String[] args) {
         int age[] = {17, 21, 97, 66, 55, 30, 33, 45, 58, 67, 38, 39, 41, 19, 23};
         Arrays.sort(age); // sort age  from min to max
-        System.out.println("---Before Sorting---:" + Arrays.toString(age));
+        System.out.println("--- Sorting the data from min to max---:" + Arrays.toString(age));
         for (int n : age) {   //convert array to integer
 
             for (int i = 0; i < n; i++) ;
