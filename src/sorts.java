d17bfd9927928eb7ec61263746c7ff195fab3090
sholaabhus <sholaabhus@gmail.com>
2021-02-16 14:01:00 +0000

    SelectionSort -update sort

--- src/sorts.java
+++ src/sorts.java
@@ -1,22 +1,51 @@
-
+//Task  3 - age
 import java.util.Arrays;
 
 public class sorts {
+    public static void InsertionSort(int array[]) {
 
-    public static void main(String[] args) {
-        int age[] = {17, 21, 97, 66, 55, 30, 33, 45, 58, 67, 38, 39, 41, 19, 23};
-        Arrays.sort(age); // sort age  from min to max
+        int num = array.length;
+        for (int i = 1; i < num; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+
+    public static void main(String args[]) {
+        int[] age = {17, 21, 97, 66, 55, 30, 33, 45, 58, 67, 38, 39, 41, 19, 23};
+        System.out.println("--- Before sorting  data " + Arrays.toString(age));
+        System.out.println();
+        InsertionSort(age);// calling InsertionSort
         System.out.println("--- Sorting the data from min to max---:" + Arrays.toString(age));
-        for (int n : age) {   //convert array to integer
+        System.out.println("" );
+        System.out.println("--- range 30-40 :" );
 
-            for (int i = 0; i < n; i++) ;
+
+            for (int i = 0; i < age.length; i++) ;
+        for (int n : age) {
             if (n >= 30 && n <= 40) {
-                System.out.println(" ");
-                System.out.println(n);
+                System.out.println(n );
+
+
             }
         }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
